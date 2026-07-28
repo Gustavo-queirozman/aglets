d255d2b410f6a8eba576ec95679ce990224cc39b
Gustavo-queirozman <gustavoqueirozmit@gmail.com>
2026-07-28 14:09:36 -0300

    Criação: validação e salvar produto

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -2,22 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SalvarProduto;
 use App\Models\Produto;
 use Illuminate\Http\Request;
 
 class ProdutoController extends Controller
 {
     function index(){
-        return Produto::all();
+        $produtos = Produto::all();
+        return response()->json($produtos, 200);
     }
 
-    function create(){}
+    //function create(){}
 
-    function store(Request $request){}
+    function store(SalvarProduto $request){
+        $produto = Produto::create($request->validated());
+        return response()->json($produto, 200);
+    }
 
     function show($id){}
 
-    function edit($id){}
+    //function edit($id){}
 
     function update(Request $request, $id){}
 
---
+++ app/Http/Requests/SalvarProduto.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class SalvarProduto extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'nome' => ['required', 'string', 'max:255'],
+            'descricao' => ['required', 'string'],
+            'preco' => ['required', 'numeric', 'min:0'],
+            'quantidade' => ['required', 'integer', 'min:0']
+        ];
+    }
+}
