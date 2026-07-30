566873fdaab95acd80821b1c6a8fc3f92b053948
Gustavo-queirozman <gustavoqueirozmit@gmail.com>
2026-07-30 13:58:42 -0300

    Correção: mostrar produto

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -3,17 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\AtualizarProduto;
-use App\Http\Requests\ListarProdutosRequest;
 use App\Http\Requests\SalvarProduto;
 use App\Models\Produto;
 
 class ProdutoController extends Controller
 {
-    public function index(ListarProdutosRequest $request)
+    public function index()
     {
-        $produtos = Produto::query()
-            ->filtrar($request->filtros())
-            ->get();
+        $produtos = Produto::all();
 
         return response()->json(['message' => 'Lista de produtos', 'produtos' => $produtos], 200);
     }
@@ -25,20 +22,24 @@ class ProdutoController extends Controller
         return response()->json(['message' => 'Produto criado com sucesso', 'produto' => $produto], 201);
     }
 
-    public function show(Produto $produto)
+    public function show(int $id)
     {
+        $produto = Produto::findOrFail($id);
+
         return response()->json(['message' => 'Produto encontrado com sucesso', 'produto' => $produto], 200);
     }
 
-    public function update(AtualizarProduto $request, Produto $produto)
+    public function update(AtualizarProduto $request, int $id)
     {
+        $produto = Produto::findOrFail($id);
         $produto->update($request->validated());
 
         return response()->json(['message' => 'Produto atualizado com sucesso', 'produto' => $produto], 200);
     }
 
-    public function destroy(Produto $produto)
+    public function destroy(int $id)
     {
+        $produto = Produto::findOrFail($id);
         $produto->delete();
 
         return response()->json(['message' => 'Produto excluido com sucesso'], 200);
