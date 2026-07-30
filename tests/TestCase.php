99fcccae6261698ff89b9bfd83635aff695348ef
Gustavo-queirozman <gustavoqueirozmit@gmail.com>
2026-07-30 15:30:43 -0300

    Implementação: cache em redis

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,35 @@
 
 namespace Tests;
 
+use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    public function createApplication()
+    {
+        $this->clearBootstrapCaches();
+
+        $app = require Application::inferBasePath().'/bootstrap/app.php';
+
+        $app->make(Kernel::class)->bootstrap();
+
+        return $app;
+    }
+
+    protected function clearBootstrapCaches(): void
+    {
+        $cacheFiles = [
+            Application::inferBasePath().'/bootstrap/cache/config.php',
+            Application::inferBasePath().'/bootstrap/cache/events.php',
+            Application::inferBasePath().'/bootstrap/cache/routes-v7.php',
+        ];
+
+        foreach ($cacheFiles as $cacheFile) {
+            if (is_file($cacheFile)) {
+                unlink($cacheFile);
+            }
+        }
+    }
 }
